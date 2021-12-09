554a3d0337f7d95dc77be6d707cf12d8830854dc
rohithp8899@gmail.com <rohitp8899@gmail.com>
2021-12-10 00:42:59 +0530

    Added app

---
+++ rohith/news_app/lib/views/news_category.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/helper/news.dart';
+import 'package:news_app/helper/widgets.dart';
+
+class CategoryNews extends StatefulWidget {
+
+  final String newsCategory;
+
+  const CategoryNews({required this.newsCategory});
+
+  @override
+  _CategoryNewsState createState() => _CategoryNewsState();
+}
+
+class _CategoryNewsState extends State<CategoryNews> {
+  var newslist;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    getNews();
+    super.initState();
+  }
+
+  void getNews() async {
+    NewsForCategorie news = NewsForCategorie();
+    await news.getNewsForCategory(widget.newsCategory);
+    newslist = news.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       Text(
+      //         "Flutter",
+      //         style:
+      //         TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
+      //       ),
+      //       Text(
+      //         "News",
+      //         style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+      //       )
+      //     ],
+      //   ),
+      //   actions: <Widget>[
+      //     Opacity(
+      //       opacity: 0,
+      //       child: Container(
+      //           padding: EdgeInsets.symmetric(horizontal: 16),
+      //           child: Icon(Icons.share,)),
+      //     )
+      //   ],
+      //   backgroundColor: Colors.transparent,
+      //   elevation: 0.0,
+      // ),
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {
+          Navigator.of(context).pop();
+        }, icon: Icon(Icons.arrow_back_ios,color: Colors.black,),),
+        title: PreferredSize(
+            preferredSize: Size.fromHeight(100.0),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 4, 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  //Text('13 Oct,2021')
+                  Text(
+                    "NewsApp",
+                    style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600,fontSize: 30),
+                  ),
+                  SizedBox(width: 20,)
+                ],
+              ),
+
+            ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: _loading ? Center(
+        child: CircularProgressIndicator(),
+      ) : SingleChildScrollView(
+        child: Container(
+          child: Container(
+            margin: EdgeInsets.only(top: 16),
+            child: ListView.builder(
+                itemCount: newslist.length,
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return NewsTile(
+                    imgUrl: newslist[index].urlToImage ?? "",
+                    title: newslist[index].title ?? "",
+                    desc: newslist[index].description ?? "",
+                    content: newslist[index].content ?? "",
+                    posturl: newslist[index].articleUrl ?? "",
+                  );
+                }),
+          ),
+        ),
+      ),
+    );
+  }
+}
